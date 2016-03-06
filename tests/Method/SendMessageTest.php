e1570960495ed950625d3796ff6ccfbc4982c2b3
Pavel Chebotarev <chebotarevp@gmail.com>
2016-03-06 03:56:53 +0300

    remove redundant tests

--- tests/Method/SendMessageTest.php
+++ tests/Method/SendMessageTest.php
@@ -7,21 +7,64 @@ use Steelbot\TelegramBotApi\Type\Message;
 
 class SendMessageTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBuildResult()
+    /**
+     * @dataProvider buildResultDataProvider
+     */
+    public function testBuildResult($data)
     {
-        $method = new SendMessage(123, "Hello");
+        $method = new SendMessage($data['chat']['id'], $data['text']);
 
-        $result = [
-            'message_id' => 123,
-            'chat' => [
-                'type' => 'private',
-                'id' => 123
-            ],
-            'date' => time()
-        ];
-        $message = $method->buildResult($result);
+        $message = $method->buildResult($data);
 
         $this->assertInstanceOf(Message::class, $message);
+        $this->assertEquals($data['message_id'], $message->messageId);
+        $this->assertEquals($data['from']['id'], $message->from->id);
+        $this->assertEquals($data['chat']['id'], $message->chat->id);
+        $this->assertEquals($data['text'], $message->text);
+    }
+
+    public function buildResultDataProvider()
+    {
+        $data = [
+            'message_id' => 4757,
+            'from' => [
+                'id' => 987654320,
+                'first_name' => 'Steel Bot',
+                'username' => 'SteelbotBot'
+            ],
+
+            'chat' => [
+                'id' => 987654321,
+                'first_name' => 'Mister',
+                'last_name' => 'Botman',
+                'username' => 'mrbotman',
+                'type' => 'private'
+            ],
+
+            'date' => 1456600086,
+            'text' => 'Hello there!'
+        ];
+
+        $data2 = [
+            'message_id' => 4757,
+            'from' => [
+                'id' => 987654320,
+                'username' => 'SteelbotBot'
+            ],
+
+            'chat' => [
+                'id' => 987654321,
+                'type' => 'private'
+            ],
+
+            'date' => 1456600086,
+            'text' => 'Hello there!'
+        ];
+
+        return [
+            [$data],
+            [$data2]
+        ];
     }
 
     function testJsonSerialize()
