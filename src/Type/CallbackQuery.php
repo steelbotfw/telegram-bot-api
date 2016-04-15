8a8cd1de8eae71ebf4c6b9bed1b51d6360ff26ef
Pavel Chebotarev <chebotarevp@gmail.com>
2016-04-15 04:02:20 +0300

    CallbackQuery type added

---
+++ src/Type/CallbackQuery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Steelbot\TelegramBotApi\Type;
+
+/**
+ * CallbackQuery
+ */
+class CallbackQuery
+{
+    /**
+     * Unique identifier for this query.
+     *
+     * @var string
+     */
+    public $id;
+
+    public $from;
+
+    public $message;
+
+    public $inlineMessageId;
+
+    public $data;
+
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->id = $data['id'];
+        $this->from = new User($data['from']);
+        $this->message = $data['message'] ? new Message($data['message']) : null;
+        $this->inlineMessageId = $data['inline_message_id'] ?? null;
+        $this->data = $data['data'];
+    }
+}
