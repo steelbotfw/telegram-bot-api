9f50c4007efdd973c64cf6a44b7e9fa8e477b80f
Pavel Chebotarev <chebotarevp@gmail.com>
2016-04-17 03:25:50 +0300

    unbanChatMember method added

---
+++ src/Method/UnbanChatMember.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Steelbot\TelegramBotApi\Method;
+
+class UnbanChatMember extends AbstractMethod
+{
+    /**
+     * @var integer|string
+     */
+    protected $chatId;
+
+    /**
+     * @var integer
+     */
+    protected $userId;
+
+    public function __construct($chatId, int $userId)
+    {
+        $this->chatId = $chatId;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getChatId()
+    {
+        return $this->chatId;
+    }
+
+    /**
+     * @param string|int $chatId
+     *
+     * @return SendLocation
+     */
+    public function setChatId($chatId)
+    {
+        $this->chatId = $chatId;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     *
+     * @return UnbanChatMember
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get method name.
+     *
+     * @return string
+     */
+    public function getMethodName(): string
+    {
+        return 'unbanChatMember';
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getHttpMethod(): string
+    {
+        return self::HTTP_POST;
+    }
+
+    /**
+     * Get parameters for HTTP query.
+     *
+     * @return mixed
+     */
+    public function getParams(): array
+    {
+        $params = [
+            'chat_id' => $this->chatId,
+            'user_id' => $this->userId
+        ];
+
+        return $params;
+    }
+
+    /**
+     * Build result type from array of data.
+     *
+     * @param bool $result
+     *
+     * @return bool
+     */
+    public function buildResult($result)
+    {
+        return $result;
+    }
+}
---
+++ tests/Method/UnbanChatMemberTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Steelbot\Tests\TelegramBotApi\Method;
+
+use Steelbot\TelegramBotApi\Method\UnbanChatMember;
+
+class UnbanChatMemberTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetParams()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $params = $method->getParams();
+
+        $this->assertArrayHasKey('chat_id', $params);
+        $this->assertEquals(123, $params['chat_id']);
+        $this->assertArrayHasKey('user_id', $params);
+        $this->assertEquals(31337, $params['user_id']);
+    }
+
+    public function testBuildResult()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $result = $method->buildResult(true);
+
+        $this->assertTrue($result);
+    }
+
+    public function testGetHttpMethod()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $this->assertEquals(UnbanChatMember::HTTP_POST, $method->getHttpMethod());
+    }
+
+    public function testGetMethodName()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $this->assertEquals('unbanChatMember', $method->getMethodName());
+    }
+
+    public function testGetSetUserId()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $method->setUserId(321);
+
+        $this->assertEquals(321, $method->getUserId());
+    }
+
+    public function testGetSetChatId()
+    {
+        $method = new UnbanChatMember(123, 31337);
+
+        $method->setChatId('@chatid');
+
+        $this->assertEquals('@chatid', $method->getChatId());
+    }
+}
