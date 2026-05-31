6b8e70645770fed92d81299dda3ce6348b163f09
Pavel Chebotarev <90736+nexor@users.noreply.github.com>
2026-05-31 20:17:00 +0300

    Added PHP CS Fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        //__DIR__ . '/examples',
+        //__DIR__ . '/src',
+        //__DIR__ . '/tests',
+        __DIR__ . '/tools',
+    ])
+    ->append([
+        __DIR__ . '/tools/codegen',
+    ])
+    ->exclude('vendor')
+    ->name('*.php');
+
+return (new Config())
+    ->setFormat('txt')
+    ->setRiskyAllowed(false)
+    ->setUsingCache(false)
+    ->setRules([
+        '@PSR12' => true,
+        'fully_qualified_strict_types' => [
+            'import_symbols' => true,
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+    ])
+    ->setFinder($finder);
--- Makefile
+++ Makefile
@@ -6,3 +6,11 @@ unit-test:
 psalm:
 	./vendor/bin/psalm
 .PHONY: psalm
+
+cs-check:
+	./vendor/bin/php-cs-fixer fix --dry-run --diff --verbose --sequential --format=txt
+.PHONY: cs-check
+
+cs-fix:
+	./vendor/bin/php-cs-fixer fix --verbose --sequential --format=txt
+.PHONY: cs-fix
